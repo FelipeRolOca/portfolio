4dfcd5253e2c99cb250041a97b8cf018bcd8064a
FelipeRolOca <felipeoca123@hotmail.com>
2026-04-10 21:59:15 -0300

    Mensaje corto

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -118,7 +118,7 @@ export function Projects() {
 
                 {/* Image Side */}
                 <div className={`relative h-64 lg:h-auto overflow-hidden ${index % 2 !== 0 ? 'lg:order-last' : ''}`}>
-                  <div className="absolute inset-0 bg-transparent z-10 group-hover:bg-transparent transition-colors duration-500" />
+                  <div className="absolute inset-0 bg-transparent z-10 transition-colors duration-500" />
                   <ImageWithFallback
                     src={project.image}
                     alt={project.title}
